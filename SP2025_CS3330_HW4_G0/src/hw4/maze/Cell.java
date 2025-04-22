5e0d031e1992222b3d2354b971a343c6d2ed1785
Borui Zhang <borui@botenders.com>
2025-04-22 18:36:10 -0500

    Parameterized Constructor and getter/setters generated

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Cell.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Cell.java
@@ -2,4 +2,28 @@ package hw4.maze;
 
 public class Cell {
 
+	private CellComponents up;
+	private CellComponents down;
+	private CellComponents left;
+	private CellComponents right;
+	
+}
+
+/**parameterized constructor for Cell, basically given in the CellTest
+ * @param left
+ * @param right
+ * @param up
+ * @param down
+ */
+public Cell(CellComponents left, CellComponents right, CellComponents up, CellComponents down) {
+
+}
+
+
+
+
+
+
+
+
 }
