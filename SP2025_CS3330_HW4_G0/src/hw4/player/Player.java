19d628f0cc2a6e786db893954468d693bb5ad817
Hao Ouyang <joe520088@gmail.com>
2025-04-22 18:18:30 -0500

    final design contain with cell and row from the maze package

--- SP2025_CS3330_HW4_G0/src/hw4/player/Player.java
+++ SP2025_CS3330_HW4_G0/src/hw4/player/Player.java
@@ -7,31 +7,44 @@ public class Player {
 	private Row currentRow;
 	private Cell currentCell;
 	
+	/*
+	 * 
+	 * @param currentRow to get which row the player is located
+	 * @param currentCell to get which cell the player is located
+	 */
+	
+	
 	public Player(Row currentRow, Cell currentCell) {
-		
+		super()
+		this.currentRow = currentRow;
+		this.currentCell = currentCelll;
 	}
-
+	
+	// get current row of player
 	public Row getCurrentRow() {
 		return currentRow;
 	}
 
 
-
+	// set row where player is located
 	public void setCurrentRow(Row currentRow) {
 		this.currentRow = currentRow;
 	}
 
 
-
+	// get current cell of player location
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
 
-
-
+	// set current cell of the player location
 	public void setCurrentCell(Cell currentCell) {
 		this.currentCell = currentCell;
 	}
-
 	
+	// return to string of the player location
+	@Override
+	public String toString() {
+		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
+	}
 }
