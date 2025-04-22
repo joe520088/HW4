9d263365a611b5f9bebb663f9860d907db2c3aa4
Hao Ouyang <joe520088@gmail.com>
2025-04-22 00:51:37 -0500

    initialize for cell left right ...ect. create setter and getter.

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Cell.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Cell.java
@@ -1,6 +1,54 @@
 package hw4.maze;
 
 public class Cell {
+	
+	private CellComponents left;
+	private CellComponents right;
+	private CellComponents up;
+	private CellComponents down;
+	
+	public Cell(CellComponents left, CellComponents right,CellComponents up,CellComponents down)
+	{
+		setLeft(left);
+		setRight(right);
+		setUp(up);
+		setDown(down);
+		
+	}
+
+	public CellComponents getLeft() {
+		return left;
+	}
+
+	public void setLeft(CellComponents left) {
+		this.left = left;
+	}
+
+	public CellComponents getRight() {
+		return right;
+	}
+
+	public void setRight(CellComponents right) {
+		this.right = right;
+	}
+
+	public CellComponents getUp() {
+		return up;
+	}
+
+	public void setUp(CellComponents up) {
+		this.up = up;
+	}
+
+	public CellComponents getDown() {
+		return down;
+	}
+
+	public void setDown(CellComponents down) {
+		this.down = down;
+	}
+	
+	
 
 	
 }
