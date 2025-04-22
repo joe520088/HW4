a4ade25bdf224efd2b13963237a2c38f7f140706
eteva <etevans2327@gmail.com>
2025-04-22 17:47:05 -0500

    Added private variables, constructors, getters, and setters to Game.java

--- SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
+++ SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
@@ -3,7 +3,27 @@ package hw4.game;
 import java.util.ArrayList;
 import java.util.Random;
 
+import hw4.maze.Grid;
+
 public class Game {
 	
+	private Grid grid;
+	private Random random = new Random();
+	
+	public Game(Grid grid) {
+		this.grid = grid;
+	}
+
+	public Game(int size) {
+		this.grid = createRandomGrid(size);
+	}
+
+	public Grid getGrid() {
+		return grid;
+	}
+
+	public void setGrid(Grid grid) {
+		this.grid = grid;
+	}
 
 }
