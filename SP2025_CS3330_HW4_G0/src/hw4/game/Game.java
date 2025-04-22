776c96dd4e25d611ced91cfbd2904889b112d899
eteva <etevans2327@gmail.com>
2025-04-22 17:48:24 -0500

    Added play method to Game.java

--- SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
+++ SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
@@ -3,6 +3,11 @@ package hw4.game;
 import java.util.ArrayList;
 import java.util.Random;
 
+import hw4.maze.Cell;
+import hw4.maze.CellComponents;
+import hw4.maze.Row;
+import hw4.player.Movement;
+import hw4.player.Player;
 import hw4.maze.Grid;
 
 public class Game {
@@ -24,6 +29,62 @@ public class Game {
 
 	public void setGrid(Grid grid) {
 		this.grid = grid;
+	}
+	
+	public boolean play(Movement movement, Player player) {
+		if(movement == null || player == null) {
+			return false;
+		}
+		
+		Row currentRow = player.getCurrentRow();
+		Cell currentCell = player.getCurrentCell();
+		
+		int rowIndex = grid.getRows().indexOf(currentRow);
+		int cellIndex = currentRow.getCells().indexOf(currentCell);
+		
+		switch(movement) {
+		case UP:
+			if(currentCell.getUp() == CellComponents.APERTURE) {
+				if(rowIndex > 0) {
+					Row newRow = grid.getRows().get(rowIndex - 1);
+					player.setCurrentRow(newRow);
+					player.setCurrentCell(newRow.getCells().get(cellIndex));
+					return true;
+				}
+			}
+			break;
+		case DOWN:
+			if(currentCell.getDown() == CellComponents.APERTURE) {
+				if(rowIndex < grid.getRows().size() - 1) {
+					Row newRow = grid.getRows().get(rowIndex + 1);
+					player.setCurrentRow(newRow);
+					player.setCurrentCell(newRow.getCells().get(cellIndex));
+					return true;
+				}
+			}
+			break;
+		case LEFT:
+			if(currentCell.getLeft() == CellComponents.APERTURE) {
+				if(cellIndex > 0) {
+					player.setCurrentCell(currentRow.getCells().get(cellIndex - 1));
+					return true;
+				}
+			}
+			else if(currentCell.getLeft() == CellComponents.EXIT){
+				return true;
+			}
+			break;
+		case RIGHT:
+			if(currentCell.getRight() == CellComponents.APERTURE) {
+				if(cellIndex < currentRow.getCells().size() - 1) {
+					player.setCurrentCell(currentRow.getCells().get(cellIndex + 1));
+					return true;
+				}
+			}
+			break;
+		}
+		
+		return false;
 	}
 
 }
