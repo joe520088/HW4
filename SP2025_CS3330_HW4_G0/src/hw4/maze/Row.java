1aaddc26b903c91b64776714e04f9eea645b7e00
Borui Zhang <borui@botenders.com>
2025-04-22 18:52:24 -0500

    Skeleton of Row with a parameterized constructor

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Row.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Row.java
@@ -2,4 +2,12 @@ package hw4.maze;
 
 public class Row {
 
+	//A row is made up from a list of cells
+	private ArrayList<Cell> cells;
+	
+	/**Parameterized constructor for a row that takes cells as input
+	 * @param cells
+	 */
+	public Row(ArrayList<Cell> cells) {
+	}
 }
