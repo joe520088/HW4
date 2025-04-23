bcf75d824f5a5e4a1ef7fc16df6c3b74d0605781
Hao Ouyang <joe520088@gmail.com>
2025-04-23 18:08:14 -0500

    this is the save version of initial declaration

--- SP2025_CS3330_HW4_G0/src/hw4/simulation/Simulation.java
+++ SP2025_CS3330_HW4_G0/src/hw4/simulation/Simulation.java
@@ -1,9 +1,23 @@
 package hw4.simulation;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import hw4.game.Game;
+import hw4.maze.Grid;
+
 public class Simulation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		int size = 3 + random.nextInt(5);
+		Game game = new Game(size);
+		Grid grid = game.getGrid();
+		
+		boolean running = true;
+		boolean exited = false;
 
 	}
 
