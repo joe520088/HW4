260e4e1bba319d382941d5d50afa479d65e970fc
Borui Zhang <borui@botenders.com>
2025-04-22 20:36:42 -0500

    Skeleton structure for grid class setup
    Field variable: list of rows that are in a grid
    and then the getters and setters automatically

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
@@ -1,5 +1,15 @@
 package hw4.maze;
 
-public class Grid {
+import java.util.ArrayList;
 
+public class Grid {
+	private ArrayList<Cell> cells;
+	
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
 }
