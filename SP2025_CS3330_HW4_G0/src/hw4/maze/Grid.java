2e209c3aeaacf46e34a41e6e5bf6e3cc145b7097
Borui Zhang <borui@botenders.com>
2025-04-22 20:57:05 -0500

    constructor finished, to string generated
    its in da name, babyyy

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
@@ -3,13 +3,24 @@ package hw4.maze;
 import java.util.ArrayList;
 
 public class Grid {
-	private ArrayList<Cell> cells;
+	private ArrayList<Row> rows;
 	
-	public ArrayList<Cell> getCells() {
-		return cells;
+	public Grid(ArrayList<Row> rows) {
+		this.rows = rows;
 	}
 
-	public void setCells(ArrayList<Cell> cells) {
-		this.cells = cells;
+	public ArrayList<Row> getRows() {
+		return rows;
 	}
+
+	public void setRows(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
+	
+	
 }
